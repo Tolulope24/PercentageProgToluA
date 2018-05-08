b7d60ad35570e6ad3ddd757b27e725819845154e
Tolulope24 <36515830+Tolulope24@users.noreply.github.com>
2018-05-08 14:36:40 -0400

    Added Codes

--- PercentageProgForm.cs
+++ PercentageProgForm.cs
@@ -17,28 +17,70 @@ namespace PercentageProgToluA
             InitializeComponent();
         }
 
-        private int ConvertToPercent (String level)
+        private int ConvertToPercent(String level)
         {
-            int Percentage;
+            // determine the integer
+            int percentage;
             switch (level)
             {
                 case "4++":
-                    Percentage = 98;
+                    percentage = 98;
                     break;
                 case "4+":
-                    Percentage = 95;
+                    percentage = 95;
                     break;
                 case "4":
-                    Percentage = 85;
+                    percentage = 85;
                     break;
-
-
+                case "4-":
+                    percentage = 80;
+                    break;
+                case "3+":
+                    percentage = 78;
+                    break;
+                case "3":
+                    percentage = 75;
+                    break;
+                case "3-":
+                    percentage = 72;
+                    break;
+                case "2+":
+                    percentage = 68;
+                    break;
+                case "2":
+                    percentage = 65;
+                    break;
+                case "2-":
+                    percentage = 62;
+                    break;
+                case "1+":
+                    percentage = 58;
+                    break;
+                case "1":
+                    percentage = 55;
+                    break;
+                    // set default
+                default:
+                    percentage = -1;
+                    break;
+                   
+                    
             }
-
+               // Return Percentage
+               return percentage;
         }
         
         private void btnCheck_Click(object sender, EventArgs e)
         {
+            // Identify the integer and String
+            string userLevel;
+            int userPercentage;
+            // Convert the grade level to a string
+            userLevel = txtGradeLevel.Text;
+            // call back the address
+            userPercentage = ConvertToPercent(userLevel);
+            // Show Message Box
+            MessageBox.Show("The Grade is : " + userPercentage + "Percent" );
 
         }
     }
